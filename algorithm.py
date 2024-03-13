64e54895fb3704f23831d16c3938bdde0b3608e0
rvle727 <rafasvoice@gmail.com>
2024-03-12 20:24:40 -0700

    Created function to concatenate company name with birthyear

--- algorithm.py
+++ algorithm.py
@@ -2,7 +2,15 @@ def remove_vowels(name):
     vowels = "aeiouAEIOU"
     return "".join([char for char in name if char not in vowels])
 
+def generate_password(step1, step2):
+    return step1 + step2 #concatenates step 1 and step 2
+
 name_user_input = input("Enter the company/website name: ")  # Prompt the user to input the company/website name
 company_name = remove_vowels(name_user_input)  
 
-print(company_name)
+print(company_name) #result of step 1
+
+birthYear = input("Enter the last 2 digits of your birth year: ")
+
+passwordv1 = generate_password(company_name, birthYear)
+print("Generated password:", passwordv1) #step 3 of the password generation. Concatenates step 1 and 2.
