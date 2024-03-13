b3c54122fe3ee9ea593c71d3e0e8c507f932a223
rvle727 <rafasvoice@gmail.com>
2024-03-12 20:05:27 -0700

    User's are able to input company names as a variable

--- algorithm.py
+++ algorithm.py
@@ -2,7 +2,7 @@ def remove_vowels(name):
     vowels = "aeiouAEIOU"
     return "".join([char for char in name if char not in vowels])
 
-spotify = "Spotify"
-company_name = remove_vowels(spotify)
+name_user_input = input("Enter the company/website name: ")  # Prompt the user to input the company/website name
+company_name = remove_vowels(name_user_input)  
 
 print(company_name)
